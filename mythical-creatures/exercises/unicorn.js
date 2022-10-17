dbed3bd2902c01f649f68b0d4e8d5b44733b0e01
Rae Gebhart <raegebhart@gmail.com>
2022-10-17 16:51:54 -0600

    test: Pass unicorn.js file tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,21 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    } 
   }
+      isWhite() {
+      if (this.color === "white") {
+      return true;
+    } 
+      return false;
+  }
+      says(statement) {
+      return `**;* ${statement} *;**`
+      }
 }
 
 module.exports = Unicorn;
