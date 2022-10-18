a69b7b50a226315375199c037b46708c31638af3
Rae Gebhart <raegebhart@gmail.com>
2022-10-17 18:28:14 -0600

    test: Pass all dragon.js tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,27 @@
+// const { test } = require("mocha")
 
+class Dragon {
+    constructor(name, rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.meal = 1;
+        console.log(this.meal)
+        console.log(this.hungry)
+    }
+    eat() {
+        var foodBreak = this.meal++
+        if (foodBreak === 3) {
+        this.hungry = false;
+        } 
+        console.log(this.meal)
+        console.log(this.hungry)
+    }
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+}
+
+// Dragon.eat();
+
+module.exports = Dragon;
