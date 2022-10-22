c5c7d2954a109854c81cb4ec6cbe8061d3b41fc1
Rae Gebhart <raegebhart@gmail.com>
2022-10-22 11:24:11 -0600

    Pass snowman tests

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,18 @@
 class Snowman {
-
+    constructor(snowmanDetails) {
+        this.carrots = snowmanDetails.carrots;
+        this.coal = snowmanDetails.coal;
+        this.buttons = snowmanDetails.buttons;
+        this.snowballs = snowmanDetails.snowballs;
+        this.magicHat = false;
+    }
+    canWearMagicHat() {
+        if (this.coal < 2 || this.buttons < 5 || this.carrots < 1 || this.snowballs < 2) {
+            return this.magicHat = false;
+        } else {
+            return this.magicHat = true;
+        }
+    }
 }
 
 module.exports = Snowman;
