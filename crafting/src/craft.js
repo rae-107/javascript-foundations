453cb6dbbe119b9d9b14dde4f80e8fed60cbd8d0
Rae Gebhart <raegebhart@gmail.com>
2022-11-06 11:54:41 -0700

    test: Pass all craft tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,19 @@
-class Craft {
+const Material = require("./material")
 
+class Craft {
+    constructor(craftObj) {
+        this.name = craftObj.type
+        this.materials = craftObj.materials
+        this.completed = false
+    }
+    completeCraft() {
+        this.completed = true
+        return 'All done! It looks great!'
+    }
+    calculateProjectTotal() {
+        var totalCraftPrice = this.materials[0].calculateMaterialCost() + this.materials[1].calculateMaterialCost()
+        return totalCraftPrice
+    }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,21 @@
 class Material {
-
+    constructor(name, pricePerUnit, amount, units) {
+        this.name = name
+        this.price = pricePerUnit
+        this.amount = amount
+        this.units = units
+    }
+    useMaterial(amountUsed) {
+        if (this.amount > amountUsed) {
+        this.amount -= amountUsed
+        return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+        } 
+        return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+    }
+    calculateMaterialCost() {
+        var totalPrice = this.price * this.amount
+        return totalPrice
+    }
 }
 
 module.exports = Material;
