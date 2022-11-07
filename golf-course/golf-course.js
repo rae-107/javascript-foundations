8db7219165b3b69df63be48b166dcb28594b6cbf
Rae Gebhart <raegebhart@gmail.com>
2022-11-07 13:05:52 -0700

    test: Pass all golf course tests

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,5 +1,22 @@
 class GolfCourse {
+    constructor(name, difficulty, openings, features) {
+        this.name = name
+        this.difficulty = difficulty
+        this.openings = openings
+        this.features = features
+        this.currentlyPlaying = []
+    }
+    checkInGroup(groups) {
+        if (this.openings > groups.length) {
+        this.openings -= groups.length
+        for (var i = 0; i < groups.length; i++) {
+            this.currentlyPlaying.unshift(groups[i].name)
+        } return 'You\'re checked in. Have fun!'
+        } else if (this.openings < groups.length) {
+            return 'Sorry, we are currently booked! Please come back later.'
+        }
 
+    }
 
 }
 
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,39 @@
 class Golfer {
-
-
+    constructor(golferObj) {
+        this.name = golferObj.name
+        this.handicap = golferObj.handicap
+        this.frustration = 0
+        this.confidence = 0
+    }
+    calculateAvg(par) {
+        var average = this.handicap + par
+        return `I usually shoot a ${average} on average.`
+    }
+    playRound(course) {
+        if (course.difficulty === 'hard') {
+            this.frustration = 500
+        } else if (course.difficulty === 'moderate') {
+            this.frustration = 100
+        }
+    }
+    hitTheRange() {
+        this.confidence += 10
+    }
+    marvel(course) {
+        return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+    }
+    whatYaShoot(score) {
+        if (score > 0) {
+            this.frustration += 20
+            return 'Doh!'
+        } else if (score === 0) {
+            this.frustration = 10
+            return 'Booyah!'
+        } else if (score < 0) {
+            this.frustration = 0
+            return 'I AM THE GREATEST GOLFER ALIVE!'
+        }
+    }
 }
 
 module.exports = Golfer;
